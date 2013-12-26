01b2e71b095f75612de76f20218f84b13b1b2c07
Rho con Linux <rhoconlinux@gmail.com>
2013-12-26 11:49:10 +0100

    / ‘theme_index/plank-on-dock-themer.sh’

--- theme_index/plank-on-dock-themer.sh
+++ theme_index/plank-on-dock-themer.sh
@@ -143,9 +143,9 @@ chmod +x $HOME/.config/plank/dock1/theme_index/*.sh
 
 #MOVING THE .DESKTOP FILES TO THE PROPER TEMP LOCATION
 #======================================================
-rm -R $HOME/.config/plank/dock1/theme_index_back/plank-themer-ondock;
-mkdir -p $HOME/.config/plank/dock1/theme_index_back/plank-themer-ondock
-cp -a $HOME/.config/plank/dock1/theme_index/*.desktop $HOME/.config/plank/dock1/theme_index_back/plank-themer-ondock
+rm -R $HOME/.config/plank/dock1/theme_index/plank-themer-ondock;
+mkdir -p $HOME/.config/plank/dock1/theme_index/plank-themer-ondock
+cp -a $HOME/.config/plank/dock1/theme_index/*.desktop $HOME/.config/plank/dock1/theme_index/plank-themer-ondock
 
 #=END==========================================================================
 #====--------BUILDING ITEMS: LIST OF THEMES AND PER-THEME-APPLYER --------=====
