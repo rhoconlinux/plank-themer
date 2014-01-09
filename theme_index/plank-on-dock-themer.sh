b177de7b82bd035c6be5c58c38d9fafe7c668381
Rho con Linux <rhoconlinux@gmail.com>
2014-01-09 14:08:09 +0100

    / ‘theme_index/plank-on-dock-themer.sh’

--- theme_index/plank-on-dock-themer.sh
+++ theme_index/plank-on-dock-themer.sh
@@ -296,7 +296,7 @@ echo "How to use:"
 echo "-----------"
 echo "1) Click on the NEW DARK ICON you now see on Plank"
 echo "2) Select your theme to change to look of the Dock"
-echo "And remember to comment if you experience anything odd ^_^"
+echo "...And remember to comment if you experience anything odd ^_^"
 #plank &
 #kill && 
 #thank you Crusty! http://ubuntuforums.org/showthread.php?t=1685823
