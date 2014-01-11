c9dd827768256b2dc45e4389959a6ca7fe7985b6
Rho con Linux <rhoconlinux@gmail.com>
2014-01-11 11:40:09 +0100

    / ‘theme_index/plank-on-dock-themer.sh’

--- theme_index/plank-on-dock-themer.sh
+++ theme_index/plank-on-dock-themer.sh
@@ -317,7 +317,7 @@ confirm () {
             ;;
     esac
 }
-confirm "Install themes (y) or finish the installation (n)"&& cd $HOME/.config/plank/dock1/themes-repo/Themes && sudo cp -v -a . /usr/share/plank/themes
+confirm "Install the themes (y) or finish the installation (n)"&& cd $HOME/.config/plank/dock1/themes-repo/Themes && sudo cp -v -a . /usr/share/plank/themes
 #http://stackoverflow.com/questions/3231804/in-bash-how-to-add-are-you-sure-y-n-to-any-command-or-alias
 
 
